50b16614b635289f4a70348bf591e8629b462711
Mariyq1 <m4697597@gmail.com>
2022-12-19 02:18:50 +0100

    Styling Wether.js

--- src/Weather.js
+++ src/Weather.js
@@ -8,28 +8,37 @@ export default function Weather(){
                 <div className="row">
                     <div className="col-9">
                 <input type="search" placeholder="Type a city"
-                className="form-control"/>
+                className="form-control"
+                autofocus="on"/>
                 </div>
                 <div className="col-3">
                 <input type="submit" value="Search"
-                className="btn btn-secondary"/>
+                className="btn btn-light"/>
                 </div>
                 </div>
             </form>
-            <h1>Weather in New York</h1>
-            <ul>
-                <li>Sunday 13:00 p.m.</li>
-                <li>Cloudy</li>
-            </ul>
-          
             <div className="row">
                 <div className="col-6">
+            <h1>New York</h1>
+            <ul>
+                <li>Sunday 13:00 p.m.</li>
+                <li>Cloudy </li>
+            </ul>
+            </div>
+            <div className="col-6">
+                   <span className="temperature">4</span> 
+                   <span className="units">°C</span>
+            </div>
+          </div>
+            <div className="row">
+                <div className="col-6">
+                  
                     <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
                     alt="Weather"/>
-                    4°C
+                    
                 </div>
                 <div className="col-6">
-                    <ul>
+                    <ul className="p-2">
                         <li>🌬︎Wind: 6,1 m/s</li>
                         <li>💧 Humidity: 53%</li>
                     </ul>
