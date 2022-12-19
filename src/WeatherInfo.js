dc5bff5dc781b404576145a9bb600eba6275f1ad
Mariyq1 <m4697597@gmail.com>
2022-12-19 20:17:50 +0100

    Convert to celsius

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,26 +1,35 @@
 import React from "react";
 import FormatedDate from "./FormatedDate";
+import WeatherTemperature from "./WeatherTemperature";
  
 export default function WeatherInfo(props){
-    return(<div className="WeatherInfo"><h1>{props.data.city}</h1>
+    return(<div className="WeatherInfo">
+        <div className="row">
+            <div className="col-6">
+        <h1>{props.data.city}</h1>
              <ul>
                 <li><FormatedDate date={props.data.date}/></li>
                 <li className="text-capitalize">{props.data.description} </li>
             </ul>
-            <div className="row">
-                <div className="col-6">
-            <span className="temperature">{Math.round(props.data.temperature)}</span> 
-                   <span className="units">°C</span>
-            <img src={props.data.icon}
-                    alt={props.data.description}
-                    className="pb-5"/>
-           </div>
-           <div className="col-6 p-3">
-                 <ul className="p-2">
+            </div>
+            <div className="col-6">
+                    <ul className="pt-5">
                         <li>🌬︎Wind: {props.data.wind} m/s</li>
                         <li>💧 Humidity: {props.data.humidity}%</li>
                     </ul>
-                    </div>
+             </div>
             </div>
-          </div>)
+            <div className="row">
+            <div className="col-6">
+                <WeatherTemperature celsius={props.data.temperature}/>
+            </div>
+            <div className="col-6">
+                 <img src={props.data.icon}
+                    alt={props.data.description}
+                 />
+            </div>
+            </div>
+                    
+                   
+          </div>);
 }
